80234ab1e85c7af1ab19ce7a2d904533b65fa54d
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2020-04-01 11:57:14 -0500

    Prueba

--- CreateThread2.java
+++ CreateThread2.java
@@ -4,30 +4,39 @@ public class CreateThread2
     public CreateThread2() {
 	super();
     }
-    
+
     // public CreateThread2(String name) {
     // 	super(name);
     // }
-    
+
     public void run() {
 	while (true) {
-	    System.out.println("Hello word from CreateThread 2");
-	    if (Thread.currentThread().isInterrupted())
-		break;
+	    try {
+		System.out.println("Hello word from CreateThread 2");
+		Thread
+		    .currentThread()
+		    .sleep(1000);
+		Thread.currentThread().interrupted();
+	    }
+	    catch (InterruptedException ie) {
+		Thread.currentThread().interrupted();
+	    }
+	    // if (Thread.currentThread().isInterrupted())
+	    // 	break;
 	}
     }
 
     public static void main(String[] args) {
 
-	Thread t = new CreateThread2();
+ 	Thread t = new CreateThread2();
 	t.setName("Mi hilo");
 	t.setPriority(10);
 	t.start();
 	System.out.println("Hilo: " + t + " estas vivo: " + t.isAlive());
 	try {
 	    Thread
-		.currentThread()
-		.sleep(2000);
+	    	.currentThread()
+	    	.sleep(2000);
 	    t.interrupt();
 	    t.join();
 	}
